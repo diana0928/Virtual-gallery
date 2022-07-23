a5f270dae524c4c5a1d0d583178309dcaa1bd550
diana <diana.pvz02@gmail.com>
2022-07-22 23:54:36 -0500

    Agregue la vista home y agregue header

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex justify-between bg-white p-8 shadow-lg text-black text-lg">
+        <div>Logo</div>
+        <div class="flex">
+        <router-link class="text-black uppercase px-2" :to="link.href" v-for="link in links" :key="link.name" v-text="link.name"></router-link>
+        </div>
+        <div>
+            <a class="btn font-bold uppercase bg-blue-500 px-10 py-3 rounded-full text-white hover:bg-white hover:text-blue-500 hover:border-2 hover:border-blue-500">Contact</a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            links : [
+                {
+                    name : 'Home',
+                    href : '/',
+                    icon : 'home'
+                },
+                {
+                    name : 'About us',
+                    href : '/about',
+                    icon : 'about'
+                }
+            ]
+        }
+    },
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="w-full h-screen">
+  <VueSlickCarousel :arrows="false" :dots="true">
+      <div>
+        <img class="h-screen w-full" src="https://initiate.alphacoders.com/images/695/cropped-1366-768-695548.jpg?5098">
+      </div>
+      <div>
+        <img class="h-screen w-full" src="https://images4.alphacoders.com/688/thumb-1920-688660.jpg">
+      </div>
+      <div>
+        <img class="h-screen w-full" src="https://images7.alphacoders.com/695/thumb-1920-695927.jpg">
+      </div>
+      <div>
+        <img class="h-screen w-full" src="https://images5.alphacoders.com/599/thumb-1920-599012.jpg">
+      </div>
+    </VueSlickCarousel>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+ import VueSlickCarousel from 'vue-slick-carousel'
+  import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    VueSlickCarousel 
   }
 }
 </script>
